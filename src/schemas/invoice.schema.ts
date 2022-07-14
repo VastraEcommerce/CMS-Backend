038eec08d9f6bd9528b6712fa4b325e795614ee1
Leeefo <a.m.abouelleef@gmail.com>
2022-07-14 10:57:50 +0200

    creating invoice and prescription record schemas

--- src/schemas/invoice.schema.ts
+++ src/schemas/invoice.schema.ts
@@ -1,8 +1,20 @@
 import { Model, Schema, Types, model, HydratedDocument } from 'mongoose';
 
 
-interface IInvoice {
-
+export interface IInvoice {
+    price: number;
+    description: string;
 }
 
-export default IInvoice
+const schema = new Schema<IInvoice>({
+    price: {
+        type: Number,
+        required: [true, "Appointment cost can not be empty"]
+    },
+    description: {
+        type: String,
+        required: [true, "Appointment description can not be empty"]
+    },
+})
+
+export default schema
---
+++ src/schemas/prescriptionRecord.schema.ts
@@ -0,0 +1,67 @@
+import { Model, Schema, Types, model, HydratedDocument } from 'mongoose';
+
+enum TimeUnit {
+    MINUTE,
+    HOUR,
+    DAY,
+    MONTH
+}
+
+interface IFrequency {
+    dose: number;
+    timeUnit: keyof typeof TimeUnit,
+    timeUnitCount: number
+}
+
+
+export interface IPrescriptionRecord {
+
+    medicine: Types.ObjectId,
+    frequency: IFrequency,
+    duration: Number,
+    foodRelationship: string
+}
+
+const freqSchema = new Schema<IFrequency>({
+    dose: {
+        type: Number,
+        default: 1,
+        required: [true, "Dose can not be empty"]
+    },
+    timeUnit: {
+        type: String,
+        enum: {
+            values: Object.values(TimeUnit),
+            message: "Time unit is not valid"
+        },
+        default: "HOUR",
+        required: [true, "Time unit can not be empty"]
+    },
+    timeUnitCount: {
+        type: Number,
+        default: 1
+    }
+}, { _id: false })
+
+const schema = new Schema<IPrescriptionRecord>({
+
+    medicine: {
+        type: Schema.Types.ObjectId,
+        ref: "Medicine",
+        required: [true, "Medicine can not be empty"]
+    },
+    frequency: {
+        type: freqSchema,
+        required: [true, "Frequency can not be empty"]
+    },
+    duration: {
+        type: Number,
+        required: [true, "Duration can not be empty"]
+    }
+}, {
+    _id: false,
+
+})
+
+
+export default schema
