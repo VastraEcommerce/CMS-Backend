b49c91fa6e79ee34dcd0dd1a935f0a805c0988c1
Leeefo <a.m.abouelleef@gmail.com>
2022-07-14 10:58:27 +0200

    modifing appointments model

--- src/models/appointments.model.ts
+++ src/models/appointments.model.ts
@@ -1,43 +1,50 @@
-import { Model, Schema, Types, model, HydratedDocument } from 'mongoose';
-import validator from 'validator';
-import IInvoice from '../schemas/invoice.schema';
-import IPrescription from '../schemas/prescription.schema';
-
+import { Model, Schema, Types, model, HydratedDocument } from "mongoose";
+import validator from "validator";
+import invoiceSchema, { IInvoice } from "../schemas/invoice.schema";
+import prescriptionRecordSchema, {
+    IPrescriptionRecord,
+} from "../schemas/prescriptionRecord.schema";
 
 interface IAppointment {
     date: Date;
-    doctor?: Types.ObjectId;
+    doctor: Types.ObjectId;
     examinationId?: Types.ObjectId | null;
-    invoice: IInvoice,
-    prescription: IPrescription,
+    invoice: IInvoice;
+    prescription: IPrescriptionRecord[];
 }
 
 interface IAppointmentMethods {
     fullName(): string;
 }
 
-interface AppointmentModel extends Model<IAppointment, {}, IAppointmentMethods> {
-    createWithFullName(name: string): Promise<HydratedDocument<IAppointment, IAppointmentMethods>>;
+interface AppointmentModel
+    extends Model<IAppointment, {}, IAppointmentMethods> {
+    createWithFullName(
+        name: string
+    ): Promise<HydratedDocument<IAppointment, IAppointmentMethods>>;
 }
 
-const schema = new Schema<IAppointment, AppointmentModel, IAppointmentMethods>({
-    date: { type: Date, required: true },
-    doctor: { type: Schema.Types.ObjectId, required: true },
-    examinationId: { type: Schema.Types.ObjectId },
-    invoice: { required: true },
-    prescription: { required: true },
-
-
-
-}, {
-    timestamps: true
-});
-schema.static('createWithFullName', function createWithFullName(name: string) {
-    const [firstName, lastName] = name.split(' ');
+const schema = new Schema<IAppointment, AppointmentModel, IAppointmentMethods>(
+    {
+        date: { type: Date, required: true },
+        doctor: { type: Schema.Types.ObjectId, ref: "Doctor", required: true },
+        examinationId: { type: Schema.Types.ObjectId, ref: "Appointment" },
+        invoice: { type: invoiceSchema, required: true },
+        prescription: [{ type: prescriptionRecordSchema, required: true }],
+    },
+    {
+        timestamps: true,
+    }
+);
+schema.static("createWithFullName", function createWithFullName(name: string) {
+    const [firstName, lastName] = name.split(" ");
     return Appointment.create({ firstName, lastName });
 });
-schema.method('fullName', function fullName(): string {
-    return this.firstName + ' ' + this.lastName;
+schema.method("fullName", function fullName(): string {
+    return this.firstName + " " + this.lastName;
 });
 
-const Appointment = model<IAppointment, AppointmentModel>('Appointment', schema);
+const Appointment = model<IAppointment, AppointmentModel>(
+    "Appointment",
+    schema
+);
