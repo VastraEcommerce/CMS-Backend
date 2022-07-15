15f1394d3f693048f4e97ff56204f2359a00e2ea
Hassan Sabry <hassan_sabry92@hotmail.com>
2022-07-15 21:29:51 +0200

    modify booking route

--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import * as bookingController from "./../controllers/booking.controller";
+import * as authController from "./../controllers/auth.controller";
 
 const router = express.Router();
-router.get("/create-checkout-session", bookingController.getCheckoutSession);
+router.get(
+  "/create-checkout-session/:id",
+  authController.login,
+  bookingController.getCheckoutSession
+);
 
 export default router;
