a2da895a3ef3e8e0fb02064527efee8afa0c75d2
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-15 06:51:33 +0200

    Add Payment controller

---
+++ src/controllers/booking.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Stripe from 'stripe';
+import Appointment from '../models/appointments.model';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2020-08-27',
+});
+
+const getCheckoutSession = expressAsyncHandler(
+  async (req: Request, res: Response) => {
+    // todo 1) Get the currently booked appointment
+    const appointment = await Appointment.findById(req.params.id);
+
+    // todo 2) Create checkout session
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: `${'name'} Appoinment`,
+              description: `${'description'}`,
+              images: ['urls'],
+            },
+            unit_amount: 100,
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: `${req.protocol}://${req.get('host')}/success.html`,
+      cancel_url: `${req.protocol}://${req.get('host')}/cancel.html`,
+    });
+
+    // todo 3) Send session as response
+    res.status(200).json({
+      status: 'success',
+      session,
+    });
+  }
+);
--- src/models/appointments.model.ts
+++ src/models/appointments.model.ts
@@ -48,3 +48,5 @@ const Appointment = model<IAppointment, AppointmentModel>(
   'Appointment',
   schema
 );
+
+export default Appointment;
