e02be227ecfbcd21855887d3ed71b4abc925be53
hassansabry0 <Hassan_sabry92@hotmail.com>
2022-07-15 17:16:00 +0200

    creating auth controller and login route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import authRouter from './auth.route';
 import doctorRouter from './doctor.route';
 import employeeRouter from './employee.route';
 import medicineRouter from './medicine.route';
@@ -8,5 +9,6 @@ const indexRouter = Router();
 indexRouter.use('/doctor', doctorRouter);
 indexRouter.use('/employee', employeeRouter);
 indexRouter.use('/medicine', medicineRouter);
+indexRouter.use('/auth', authRouter);
 
 export default indexRouter;
