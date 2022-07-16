c20df18a149e86f69b4959a9d1df75fda66b8fa6
Leeefo <a.m.abouelleef@gmail.com>
2022-07-16 07:56:50 +0200

    adding medicine routes and controllers

--- src/controllers/medicine.controller.ts
+++ src/controllers/medicine.controller.ts
@@ -23,6 +23,32 @@ export const createMedicine = asyncHandler(async (req: Request, res: Response, n
         message: "Success", data:
             "Created"
     })
+})
 
+export const getMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
 
+    const id = req.params.id
+    const medicine = await Medicine.findById(id)
+    res.status(201).json({
+        message: "Success", data: medicine
+    })
+})
+export const updateMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+    const id = req.params.id
+    const medicine = req.body
+    await Medicine.findByIdAndUpdate(id, medicine, { runValidators: true, })
+    res.status(201).json({
+        message: "Success", data:
+            "Updated"
+    })
+})
+export const deleteMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+    const id = req.params.id
+    await Medicine.findByIdAndDelete(id)
+    res.status(201).json({
+        message: "Success", data:
+            "Deleted"
+    })
 })
--- src/routes/medicine.route.ts
+++ src/routes/medicine.route.ts
@@ -2,10 +2,19 @@ import express from 'express';
 import {
   getAllMedicines,
   createMedicine,
+  getMedicine,
+  updateMedicine,
+  deleteMedicine
 } from '../controllers/medicine.controller';
+import { admin } from '../middlewares/auth/protect.middleware';
 
 const medicineRouter = express.Router();
 
-medicineRouter.route('/').get(getAllMedicines).post(createMedicine);
+medicineRouter.route('/').get(getAllMedicines).post(admin, createMedicine);
+medicineRouter
+  .route('/:id')
+  .get(getMedicine)
+  .put(admin, updateMedicine)
+  .delete(admin, deleteMedicine);
 
 export default medicineRouter;
