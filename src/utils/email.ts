fc249a11a55410ca4db289626110ba34d61ce631
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-17 22:39:14 +0200

    Add email sender class

---
+++ src/utils/email.ts
@@ -0,0 +1,74 @@
+import { htmlToText } from 'html-to-text';
+import nodemailer from 'nodemailer';
+import { renderFile } from 'pug';
+interface IEmail {
+  to: string;
+  firstName: string;
+  url: string;
+  from: string;
+}
+
+interface IUser {
+  email: string;
+  name: string;
+}
+
+export default class Email implements IEmail {
+  to: string;
+  firstName: string;
+  url: string;
+  from: string;
+
+  constructor(user: IUser, url: string) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Vastra Team <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Need Authentcation from `SendGrid` for production
+      /* return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      }); */
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: +process.env.EMAIL_PORT!,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template: 'invoice', subject: string) {
+    // todo 1) Render HTML based on a pug template
+    const html = renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      usr: this.url,
+      subject,
+    });
+
+    // todo 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+    };
+
+    // todo 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendInvoice() {
+    await this.send('invoice', 'Your invoice for appointment');
+  }
+}
