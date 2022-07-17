677be4c6f822067805147da8acd44a91bcab361a
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-17 22:46:44 +0200

    FIx confilict

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,18 +5,11 @@ import doctorRouter from './doctor.route';
 import employeeRouter from './employee.route';
 import medicineRouter from './medicine.route';
 import appointmentRouter from './appointment.route';
-<<<<<<< HEAD
 import bookingRoute from './../routes/booking.route';
 import patientRoute from './../routes/patient.route';
-=======
-import bookingRoute from "./../routes/booking.route";
-import patientRoute from "./../routes/patient.route";
-import reportsRouter from "./../routes/reports.route";
-
->>>>>>> 7dfc537a39a84c7e1c0715f329ec5c06d1039d57
+import reportsRouter from './../routes/reports.route';
 
 const indexRouter = Router();
-
 
 // Public
 indexRouter.use('/auth', authRouter);
@@ -27,13 +20,8 @@ indexRouter.use('/doctor', doctorRouter);
 indexRouter.use('/employee', employeeRouter);
 indexRouter.use('/medicine', medicineRouter);
 indexRouter.use('/appointment', appointmentRouter);
-<<<<<<< HEAD
 indexRouter.use('/booking', bookingRoute);
 indexRouter.use('/patient', patientRoute);
-=======
-indexRouter.use("/booking", bookingRoute);
-indexRouter.use("/patient", patientRoute);
-indexRouter.use("/reports", reportsRouter);
->>>>>>> 7dfc537a39a84c7e1c0715f329ec5c06d1039d57
+indexRouter.use('/reports', reportsRouter);
 
 export default indexRouter;
