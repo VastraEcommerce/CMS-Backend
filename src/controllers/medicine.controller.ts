729399a56df04ecb7e1e07b735b3c77a01994290
Hassan Sabry <hassan_sabry92@hotmail.com>
2022-07-20 20:10:45 +0200

    add search feature to medicine

--- src/controllers/medicine.controller.ts
+++ src/controllers/medicine.controller.ts
@@ -1,54 +1,66 @@
-import { NextFunction, Request, Response } from 'express'
-import asyncHandler from 'express-async-handler'
-import Medicine from '../models/medicine.model'
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import Medicine from "../models/medicine.model";
 
-
-export const getAllMedicines = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const medicines = await Medicine.find({});
+export const getAllMedicines = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let medicines;
+    const { name } = req.query;
+    if (name) {
+      medicines = await Medicine.find({
+        name: { $regex: name, $options: "$i" },
+      });
+    } else {
+      medicines = await Medicine.find({});
+    }
     res.status(200).json({
-        message: "Success",
-        data: {
-            medicines
-        }
-    })
-
-})
-export const createMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const body = req.body
-    console.log(body)
-    await Medicine.create(body)
+      message: "Success",
+      data: {
+        medicines,
+      },
+    });
+  }
+);
+export const createMedicine = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body;
+    console.log(body);
+    await Medicine.create(body);
     res.status(201).json({
-        message: "Success", data:
-            "Created"
-    })
-})
+      message: "Success",
+      data: "Created",
+    });
+  }
+);
 
-export const getMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const id = req.params.id
-    const medicine = await Medicine.findById(id)
+export const getMedicine = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const medicine = await Medicine.findById(id);
     res.status(201).json({
-        message: "Success", data: medicine
-    })
-})
-export const updateMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const id = req.params.id
-    const medicine = req.body
-    await Medicine.findByIdAndUpdate(id, medicine, { runValidators: true, })
+      message: "Success",
+      data: medicine,
+    });
+  }
+);
+export const updateMedicine = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const medicine = req.body;
+    await Medicine.findByIdAndUpdate(id, medicine, { runValidators: true });
     res.status(201).json({
-        message: "Success", data:
-            "Updated"
-    })
-})
-export const deleteMedicine = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const id = req.params.id
-    await Medicine.findByIdAndDelete(id)
+      message: "Success",
+      data: "Updated",
+    });
+  }
+);
+export const deleteMedicine = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    await Medicine.findByIdAndDelete(id);
     res.status(201).json({
-        message: "Success", data:
-            "Deleted"
-    })
-})
+      message: "Success",
+      data: "Deleted",
+    });
+  }
+);
