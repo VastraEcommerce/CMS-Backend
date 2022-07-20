8ade47f6fedb4e4e1e95262824f46a255943eabf
Hassan Sabry <hassan_sabry92@hotmail.com>
2022-07-20 19:54:18 +0200

    add search feature to employee

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -1,58 +1,65 @@
 import Employee from "../models/employee.model";
-import { NextFunction, Request, Response } from 'express';
-import asyncHandler from 'express-async-handler';
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
 
+export const getAllEmployees = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    let employees;
+    let { name } = req.query;
+    if (name) {
+      employees = await Employee.find({
+        "profile.name": { $regex: name, $options: "$i" },
+      });
+    } else {
+      employees = await Employee.find({});
+    }
 
-export const getAllEmployees = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const employees = await Employee.find({});
     res.status(200).json({
-        message: "Success",
-        data: {
-            employees
-        }
+      message: "Success",
+      data: {
+        employees,
+      },
     });
+  }
+);
 
-});
-
-export const getEmployee = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+export const getEmployee = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
     const employee = await Employee.findById(id);
     res.status(200).json({
-        message: "Success",
-        data: {
-            employee
-        }
+      message: "Success",
+      data: {
+        employee,
+      },
     });
-});
+  }
+);
 
-export const createEmployee = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
+export const createEmployee = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     await Employee.create(req.body);
     res.status(201).json({
-        message: "Success",
-        data: "Created",
-
+      message: "Success",
+      data: "Created",
     });
-
-});
-export const updateEmployee = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
+  }
+);
+export const updateEmployee = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     await Employee.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({
-        message: "Success",
-        data: "Updated",
-
+      message: "Success",
+      data: "Updated",
     });
-
-});
-export const deleteEmployee = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
+  }
+);
+export const deleteEmployee = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     await Employee.findByIdAndDelete(req.params.id);
     res.status(200).json({
-        message: "Success",
-        data: "Deleted",
-
+      message: "Success",
+      data: "Deleted",
     });
-
-});
+  }
+);
