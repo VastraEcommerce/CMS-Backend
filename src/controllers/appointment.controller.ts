8c27dfb2ede1908ab42235b538782315493d9b7e
Hassan Sabry <hassan_sabry92@hotmail.com>
2022-07-20 20:11:45 +0200

    update appointment

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -1,64 +1,70 @@
-import { NextFunction, Request, Response } from 'express'
-import asyncHandler from 'express-async-handler'
-import Appointment from '../models/appointment.model'
+import { NextFunction, Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import Appointment from "../models/appointment.model";
 
-
-export const getAllAppointments = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
+export const getAllAppointments = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
     const appointments = await Appointment.find({});
     res.status(200).json({
-        message: "Success",
-        data: {
-            appointments
-        }
-    })
-
-})
-export const createAppointment = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const body = req.body
-    console.log(body)
-    await Appointment.create(body)
+      message: "Success",
+      data: {
+        appointments,
+      },
+    });
+  }
+);
+export const createAppointment = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body;
+    console.log(body);
+    await Appointment.create(body);
     res.status(201).json({
-        message: "Success", data:
-            "Created"
-    })
-})
+      message: "Success",
+      data: "Created",
+    });
+  }
+);
 
-export const getAppointment = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const id = req.params.id
-    const appointment = await Appointment.findById(id)
+export const getAppointment = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const appointment = await Appointment.findById(id);
     res.status(201).json({
-        message: "Success", data: appointment
-    })
-})
-export const updateAppointment = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const id = req.params.id
-    const appointment = req.body
-    await Appointment.findByIdAndUpdate(id, appointment, { runValidators: true, })
+      message: "Success",
+      data: appointment,
+    });
+  }
+);
+export const updateAppointment = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const appointment = req.body;
+    await Appointment.findByIdAndUpdate(id, appointment, {
+      runValidators: true,
+    });
     res.status(201).json({
-        message: "Success", data:
-            "Updated"
-    })
-})
-export const deleteAppointment = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const id = req.params.id
-    await Appointment.findByIdAndDelete(id)
+      message: "Success",
+      data: "Updated",
+    });
+  }
+);
+export const deleteAppointment = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    await Appointment.findByIdAndDelete(id);
     res.status(201).json({
-        message: "Success", data:
-            "Deleted"
-    })
-})
-export const getAppointmentsByDate = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-
-    const date = req.params.date
-    const aps = await Appointment.getAppointmentsByDate(new Date(date))
+      message: "Success",
+      data: "Deleted",
+    });
+  }
+);
+export const getAppointmentsByDate = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const date = req.params.date;
+    const aps = await Appointment.getAppointmentsByDate(new Date(date));
     res.status(201).json({
-        message: "Success",
-        data:
-            aps
-    })
-})
+      message: "Success",
+      data: aps,
+    });
+  }
+);
